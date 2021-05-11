a39047421f3cf593eb3890d59d48f93a262f9a58
hf <yuhf1511@126.com>
2021-05-11 17:00:18 +0800

    first commit

---
+++ src/main/java/com/hf/git/MyGitApp.java
@@ -0,0 +1,11 @@
+package com.hf.git;
+
+/**
+ * @author yuhf
+ * @date 2021-05-11 16:53
+ */
+public class MyGitApp {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
